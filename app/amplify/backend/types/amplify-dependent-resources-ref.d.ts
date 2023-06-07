a3feb18929f76bb8657f72c6baae63ea918c5c5f
ninjawill543 <hansonlucas@proton.me>
2023-06-07 18:11:34 +0200

    fixing

--- app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -5,17 +5,5 @@ export type AmplifyDependentResourcesAttributes = {
       "GraphQLAPIIdOutput": "string",
       "GraphQLAPIKeyOutput": "string"
     }
-  },
-  "auth": {
-    "app1ae7e1f7": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "CreatedSNSRole": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    }
   }
 }
