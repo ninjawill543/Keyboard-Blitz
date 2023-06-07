6fd4417169fe2ce08cc40d639a7e96dd3b3daa7f
ninjawill543 <hansonlucas@proton.me>
2023-06-07 18:34:48 +0200

    amplify broke everything

--- app/App.js
+++ app/App.js
@@ -1,9 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { Amplify } from 'aws-amplify';
-import awsExports from './src/aws-exports';
-Amplify.configure(awsExports);
-
 
 export default function App() {
   return (
