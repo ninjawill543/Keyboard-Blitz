abce853e4f8750b69543573a634e0a983ed6766c
ninjawill543 <hansonlucas@proton.me>
2023-06-12 16:40:10 +0200

    home page coming along

--- app/screens/Welcome.tsx
+++ app/screens/Welcome.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View, Button, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 import colors from '../colors'
 import BlueButton from '../components/BlueButton'
@@ -8,6 +8,9 @@ const Welcome = () => {
   return (
     <View style={styles.container}>
         <View style={styles.main}>
+            <Image source={require('../assets/home.png')} style={styles.image}/>           
+            <Text style={styles.title}>Welcome to Racer </Text>
+            <Text style={styles.littleText}>The competitive typing game where you compete against your friends</Text>
             <BlueButton buttonText={'Log In'} bottomPadding={'6.67%'}/>
             <WhiteButton buttonText={"I'm new, sign me up"} bottomPadding={'26%'}/>
         </View>
@@ -32,5 +35,24 @@ const styles = StyleSheet.create({
         // paddingBottom: '26%',
 
     },
+    littleText:{
+      fontSize: 18,
+      fontFamily: 'Al Nile',
+      textAlign: 'center',
+      marginBottom: '7.36%'
+      
+    },
+    title:{
+      fontSize: 30,
+      fontFamily: 'AlNile-Bold',
+      textAlign: 'center',
+      marginBottom: '7.8%'
+    },
+    image:{
+      width: 362, 
+      height: 291.77, 
+      alignSelf: 'flex-start', 
+      marginBottom: '17.48%'
+    }
 
 })
