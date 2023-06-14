ab5df30f643b6527fa2e29052277fb2e33581f5d
ninjawill543 <hansonlucas@proton.me>
2023-06-14 16:43:41 +0200

    not sure about friends page colors

--- app/screens/Friends.tsx
+++ app/screens/Friends.tsx
@@ -10,34 +10,36 @@ import {
 const Friends = () => {
   const data = [
     { id: '1', name: 'Helen', points: '1058 pts', image: require('../assets/person1.png') },
-    { id: '2', name: 'Helen', points: '1058 pts', image: require('../assets/person1.png') },
-    { id: '3', name: 'Helen', points: '1058 pts', image: require('../assets/person1.png') },
-    { id: '4', name: 'Helen', points: '1058 pts', image: require('../assets/person1.png') },
-    { id: '5', name: 'Helen', points: '1058 pts', image: require('../assets/person1.png') },
-    { id: '6', name: 'Helen', points: '1058 pts', image: require('../assets/person1.png') },
-    { id: '7', name: 'Helen', points: '1058 pts', image: require('../assets/person1.png') },
-    { id: '8', name: 'Helen', points: '1058 pts', image: require('../assets/person1.png') },
+    { id: '2', name: 'Helen', points: '1058 pts', image: require('../assets/person2.png') },
+    { id: '3', name: 'Helen', points: '1058 pts', image: require('../assets/person3.png') },
+    { id: '4', name: 'Helen', points: '1058 pts', image: require('../assets/person4.png') },
+    { id: '5', name: 'Helen', points: '1058 pts', image: require('../assets/person5.png') },
+    { id: '6', name: 'Helen', points: '1058 pts', image: require('../assets/person6.png') },
+    { id: '7', name: 'Helen', points: '1058 pts', image: require('../assets/person7.png') },
+    { id: '8', name: 'Helen', points: '1058 pts', image: require('../assets/person8.png') },
     { id: '9', name: 'Helen', points: '1058 pts', image: require('../assets/person1.png') },
-    { id: '10', name: 'Helen', points: '1058 pts', image: require('../assets/person1.png') },
-    { id: '11', name: 'Helen', points: '1058 pts', image: require('../assets/person1.png') },
-    { id: '12', name: 'Helen', points: '1058 pts', image: require('../assets/person1.png') },
-    { id: '13', name: 'Helen', points: '1058 pts', image: require('../assets/person1.png') },
-    { id: '14', name: 'Helen', points: '1058 pts', image: require('../assets/person1.png') },
-    { id: '15', name: 'Helen', points: '1058 pts', image: require('../assets/person1.png') },
-    { id: '16', name: 'Helen', points: '1058 pts', image: require('../assets/person1.png') },
+    { id: '10', name: 'Helen', points: '1058 pts', image: require('../assets/person2.png') },
+    { id: '11', name: 'Helen', points: '1058 pts', image: require('../assets/person3.png') },
+    { id: '12', name: 'Helen', points: '1058 pts', image: require('../assets/person4.png') },
+    { id: '13', name: 'Helen', points: '1058 pts', image: require('../assets/person5.png') },
+    { id: '14', name: 'Helen', points: '1058 pts', image: require('../assets/person6.png') },
+    { id: '15', name: 'Helen', points: '1058 pts', image: require('../assets/person7.png') },
+    { id: '16', name: 'Helen', points: '1058 pts', image: require('../assets/person8.png') },
     { id: '17', name: 'Helen', points: '1058 pts', image: require('../assets/person1.png') },
-    { id: '18', name: 'Helen', points: '1058 pts', image: require('../assets/person1.png') },
-    { id: '19', name: 'Helen', points: '1058 pts', image: require('../assets/person1.png') },
+    { id: '18', name: 'Helen', points: '1058 pts', image: require('../assets/person2.png') },
+    { id: '19', name: 'Helen', points: '1058 pts', image: require('../assets/person3.png') },
     // Add more card items as needed
   ];
 
   const renderCard = ({ item }) => {
     return (
-      <View style={styles.card}>
-        <Image source={item.image} style={styles.cardImage} />
-        <Text style={styles.cardText}>{item.name}</Text>
-        <Text style={styles.cardText}>{item.points}</Text>
-      </View>
+      <TouchableOpacity>
+        <View style={styles.card}>
+          <Image source={item.image} style={styles.cardImage} />
+          <Text style={styles.cardText}>{item.name}</Text>
+          <Text style={styles.cardText}>{item.points}</Text>
+        </View>
+      </TouchableOpacity>
     );
   };
 
@@ -85,9 +87,9 @@ const styles = StyleSheet.create({
   },
   topMenu:{
     width: '100%',
-    height: 64,
+    height: 100,
     // backgroundColor: 'blue',
-    marginTop: 80,
+    marginTop: 40,
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center'
