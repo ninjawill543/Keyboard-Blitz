ca1b712bf39f26a78d8fe406c59a065a2f21094e
ninjawill543 <hansonlucas@proton.me>
2023-06-15 19:44:02 +0200

    font being weird

--- app/screens/FriendSearch.tsx
+++ app/screens/FriendSearch.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, SafeAreaView, Image, TextInput, TouchableOpacity, FlatList } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, Image, TextInput, TouchableOpacity, FlatList, Keyboard } from 'react-native'
 import React from 'react'
 import {
   useFonts,
@@ -10,35 +10,38 @@ import {
 const Email = () => {
 
   const data = [
-    { id: '1', name: 'Helen', points: '1058 pts', image: require('../assets/person1.png') },
-    { id: '2', name: 'Helen', points: '1058 pts', image: require('../assets/person2.png') },
-    { id: '3', name: 'Helen', points: '1058 pts', image: require('../assets/person3.png') },
-    { id: '4', name: 'Helen', points: '1058 pts', image: require('../assets/person4.png') },
-    { id: '5', name: 'Helen', points: '1058 pts', image: require('../assets/person5.png') },
-    { id: '6', name: 'Helen', points: '1058 pts', image: require('../assets/person6.png') },
-    { id: '7', name: 'Helen', points: '1058 pts', image: require('../assets/person7.png') },
-    { id: '8', name: 'Helen', points: '1058 pts', image: require('../assets/person8.png') },
-    { id: '9', name: 'Helen', points: '1058 pts', image: require('../assets/person1.png') },
-    { id: '10', name: 'Helen', points: '1058 pts', image: require('../assets/person2.png') },
-    { id: '11', name: 'Helen', points: '1058 pts', image: require('../assets/person3.png') },
-    { id: '12', name: 'Helen', points: '1058 pts', image: require('../assets/person4.png') },
-    { id: '13', name: 'Helen', points: '1058 pts', image: require('../assets/person5.png') },
-    { id: '14', name: 'Helen', points: '1058 pts', image: require('../assets/person6.png') },
-    { id: '15', name: 'Helen', points: '1058 pts', image: require('../assets/person7.png') },
-    { id: '16', name: 'Helen', points: '1058 pts', image: require('../assets/person8.png') },
-    { id: '17', name: 'Helen', points: '1058 pts', image: require('../assets/person1.png') },
-    { id: '18', name: 'Helen', points: '1058 pts', image: require('../assets/person2.png') },
-    { id: '19', name: 'Helen', points: '1058 pts', image: require('../assets/person3.png') },
+    { id: '1', name: 'Alice', points: '500 pts', image: require('../assets/person1.png') },
+    { id: '2', name: 'Bob', points: '750 pts', image: require('../assets/person2.png') },
+    { id: '3', name: 'Charlie', points: '300 pts', image: require('../assets/person3.png') },
+    { id: '4', name: 'David', points: '900 pts', image: require('../assets/person4.png') },
+    { id: '5', name: 'Eva', points: '600 pts', image: require('../assets/person5.png') },
+    { id: '6', name: 'Frank', points: '450 pts', image: require('../assets/person6.png') },
+    { id: '7', name: 'Grace', points: '800 pts', image: require('../assets/person7.png') },
+    { id: '8', name: 'Henry', points: '350 pts', image: require('../assets/person8.png') },
+    { id: '9', name: 'Ivy', points: '550 pts', image: require('../assets/person1.png') },
+    { id: '10', name: 'Jack', points: '700 pts', image: require('../assets/person2.png') },
+    { id: '11', name: 'Kate', points: '400 pts', image: require('../assets/person3.png') },
+    { id: '12', name: 'Liam', points: '850 pts', image: require('../assets/person4.png') },
+    { id: '13', name: 'Mia', points: '250 pts', image: require('../assets/person5.png') },
+    { id: '14', name: 'Noah', points: '950 pts', image: require('../assets/person6.png') },
+    { id: '15', name: 'Olivia', points: '600 pts', image: require('../assets/person7.png') },
+    { id: '16', name: 'Peter', points: '400 pts', image: require('../assets/person8.png') },
+    { id: '17', name: 'Quinn', points: '550 pts', image: require('../assets/person1.png') },
+    { id: '18', name: 'Ryan', points: '700 pts', image: require('../assets/person2.png') },
+    { id: '19', name: 'Sophia', points: '350 pts', image: require('../assets/person3.png') },
     // Add more card items as needed
   ];
+  
 
   const renderCard = ({ item }) => {
     return (
       <TouchableOpacity>
         <View style={styles.card}>
-          {/* <Image source={item.image} style={styles.cardImage} />
-          <Text style={styles.cardText}>{item.name}</Text>
-          <Text style={styles.cardText}>{item.points}</Text> */}
+          <Image source={item.image} style={styles.cardImage} />
+          <View>
+            <Text style={styles.cardName}>{item.name}</Text>
+            <Text style={styles.cardPTS}>{item.points}</Text>
+          </View>
         </View>
       </TouchableOpacity>
     );
@@ -58,7 +61,7 @@ const Email = () => {
               <Image source={require('../assets/search.png')} style={{width: 25, height: 25, marginLeft: 5}} />
               <TextInput placeholder='Search' placeholderTextColor='#8E8E92' style={styles.textInput}></TextInput> 
             </View>
-            <Text style={styles.cancel}>Cancel</Text>
+            <Text onPress={Keyboard.dismiss} style={styles.cancel}>Cancel</Text>
           </View>
           <View style={styles.underline} />
           {/* <View style={styles.cardContainer}>
@@ -69,7 +72,8 @@ const Email = () => {
           keyExtractor={(item) => item.id}
           numColumns={1}
           renderItem={renderCard}
-          contentContainerStyle={styles.scrollMenu}
+          // contentContainerStyle={styles.scrollMenu}
+          style={styles.scrollMenu}
         />
         </View>
     </SafeAreaView>
@@ -124,35 +128,32 @@ const styles = StyleSheet.create({
       marginBottom: 5
       
     },
-    // cardContainer:{
-    //   width: '90%',
-    //   height: '7%',
-    //   backgroundColor: 'green'
-    // },
     scrollMenu:{
       marginTop: 20,
-      // justifyContent: 'center',
-      alignItems: 'center',
-      // backgroundColor: 'red',
-  
+      width: '90%'
     },
     card:{
-      width: 400,
-      backgroundColor: 'green',
-      // marginHorizontal: 30,
-      marginBottom: 10,
-      height: 50,
-      // // backgroundColor: 'yellow',
-      // justifyContent: 'flex-start',
-      // alignItems: 'center',
+      width: '100%',
+      // backgroundColor: 'green',
+      marginBottom: 15,
+      height: 55,
+      flexDirection: 'row',
+      alignItems: 'center'
     },
     cardImage:{
-      width: 150,
-      height: 150,
-      borderRadius: 25,
+      width: 55,
+      height: 55,
+      borderRadius: 50,
     },
-    cardText:{
+    cardName:{
       fontFamily: 'Bold',
-      fontSize: 16
+      fontSize: 16,
+      marginLeft: 20
+
+    },
+    cardPTS:{
+      fontFamily: 'Regular',
+      fontSize: 14,
+      marginLeft: 20
     }
 })
