6b24a700a06c1c90e923b3a4ebd6f74b70bd5bd4
ninjawill543 <hansonlucas@proton.me>
2023-06-15 19:31:52 +0200

    making friend card

--- app/screens/FriendSearch.tsx
+++ app/screens/FriendSearch.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, SafeAreaView, Image, TextInput } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, Image, TextInput, TouchableOpacity, FlatList } from 'react-native'
 import React from 'react'
 import {
   useFonts,
@@ -8,6 +8,43 @@ import {
 } from '@expo-google-fonts/roboto';
 
 const Email = () => {
+
+  const data = [
+    { id: '1', name: 'Helen', points: '1058 pts', image: require('../assets/person1.png') },
+    { id: '2', name: 'Helen', points: '1058 pts', image: require('../assets/person2.png') },
+    { id: '3', name: 'Helen', points: '1058 pts', image: require('../assets/person3.png') },
+    { id: '4', name: 'Helen', points: '1058 pts', image: require('../assets/person4.png') },
+    { id: '5', name: 'Helen', points: '1058 pts', image: require('../assets/person5.png') },
+    { id: '6', name: 'Helen', points: '1058 pts', image: require('../assets/person6.png') },
+    { id: '7', name: 'Helen', points: '1058 pts', image: require('../assets/person7.png') },
+    { id: '8', name: 'Helen', points: '1058 pts', image: require('../assets/person8.png') },
+    { id: '9', name: 'Helen', points: '1058 pts', image: require('../assets/person1.png') },
+    { id: '10', name: 'Helen', points: '1058 pts', image: require('../assets/person2.png') },
+    { id: '11', name: 'Helen', points: '1058 pts', image: require('../assets/person3.png') },
+    { id: '12', name: 'Helen', points: '1058 pts', image: require('../assets/person4.png') },
+    { id: '13', name: 'Helen', points: '1058 pts', image: require('../assets/person5.png') },
+    { id: '14', name: 'Helen', points: '1058 pts', image: require('../assets/person6.png') },
+    { id: '15', name: 'Helen', points: '1058 pts', image: require('../assets/person7.png') },
+    { id: '16', name: 'Helen', points: '1058 pts', image: require('../assets/person8.png') },
+    { id: '17', name: 'Helen', points: '1058 pts', image: require('../assets/person1.png') },
+    { id: '18', name: 'Helen', points: '1058 pts', image: require('../assets/person2.png') },
+    { id: '19', name: 'Helen', points: '1058 pts', image: require('../assets/person3.png') },
+    // Add more card items as needed
+  ];
+
+  const renderCard = ({ item }) => {
+    return (
+      <TouchableOpacity>
+        <View style={styles.card}>
+          {/* <Image source={item.image} style={styles.cardImage} />
+          <Text style={styles.cardText}>{item.name}</Text>
+          <Text style={styles.cardText}>{item.points}</Text> */}
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
+
   return (
     useFonts({
       Regular,
@@ -24,6 +61,16 @@ const Email = () => {
             <Text style={styles.cancel}>Cancel</Text>
           </View>
           <View style={styles.underline} />
+          {/* <View style={styles.cardContainer}>
+
+          </View> */}
+          <FlatList
+          data={data}
+          keyExtractor={(item) => item.id}
+          numColumns={1}
+          renderItem={renderCard}
+          contentContainerStyle={styles.scrollMenu}
+        />
         </View>
     </SafeAreaView>
   )
@@ -74,6 +121,38 @@ const styles = StyleSheet.create({
       height: 1,
       backgroundColor: '#DBDBDB',
       width: '100%',
+      marginBottom: 5
       
     },
+    // cardContainer:{
+    //   width: '90%',
+    //   height: '7%',
+    //   backgroundColor: 'green'
+    // },
+    scrollMenu:{
+      marginTop: 20,
+      // justifyContent: 'center',
+      alignItems: 'center',
+      // backgroundColor: 'red',
+  
+    },
+    card:{
+      width: 400,
+      backgroundColor: 'green',
+      // marginHorizontal: 30,
+      marginBottom: 10,
+      height: 50,
+      // // backgroundColor: 'yellow',
+      // justifyContent: 'flex-start',
+      // alignItems: 'center',
+    },
+    cardImage:{
+      width: 150,
+      height: 150,
+      borderRadius: 25,
+    },
+    cardText:{
+      fontFamily: 'Bold',
+      fontSize: 16
+    }
 })
