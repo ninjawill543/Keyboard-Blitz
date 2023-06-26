cf88ea2f801ec7e202e8ca03d4f850790be75fe0
ninjawill543 <hansonlucas@proton.me>
2023-06-26 17:22:07 +0200

    updated json language files

--- app/imports.ts
+++ app/imports.ts
@@ -1,32 +1,66 @@
 import {
-    StyleSheet,
-    Text,
-    View,
-    Button,
-    TouchableOpacity,
-    Image,
-    KeyboardAvoidingView,
-    Platform, TextInput,
-    FlatList,
-    SafeAreaView, Keyboard, ImageBackground
-  } from "react-native";
-  import React, { useState, useEffect, useRef } from "react";
-  import colors from "./colors";
-  import BlueButton from "./components/BlueButton";
-  import WhiteButton from "./components/WhiteButton";
-  import "./locales/index";
-  import { useTranslation } from "react-i18next";
-  import {useNavigation} from '@react-navigation/native';
-  import {StackNavigationProp} from '@react-navigation/stack';
-  import {RootStackParamList} from './screens/RootStackParams';
-  import AppLoading from 'expo-app-loading';
-  import ColoredPlaceholderTextInput from "./components/ColoredText";
-  
-  import {
-    useFonts,
-    Roboto_400Regular as Regular,
-    Roboto_500Medium as Medium,
-    Roboto_700Bold as Bold,
-  } from "@expo-google-fonts/roboto";
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TouchableOpacity,
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  TextInput,
+  FlatList,
+  SafeAreaView,
+  Keyboard,
+  ImageBackground,
+} from "react-native";
+import React, { useState, useEffect, useRef } from "react";
+import colors from "./colors";
+import BlueButton from "./components/BlueButton";
+import WhiteButton from "./components/WhiteButton";
+import "./locales/index";
+import { useTranslation } from "react-i18next";
+import { useNavigation } from "@react-navigation/native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { RootStackParamList } from "./screens/RootStackParams";
+import AppLoading from "expo-app-loading";
+import ColoredPlaceholderTextInput from "./components/ColoredText";
 
-  export {ColoredPlaceholderTextInput, ImageBackground, Keyboard, SafeAreaView, FlatList, TextInput, Platform, useRef,KeyboardAvoidingView, Regular, Medium, Bold, useFonts, StyleSheet,Text,View,Button, TouchableOpacity,Image, React, useState, useEffect, colors, BlueButton, WhiteButton, useTranslation, useNavigation, StackNavigationProp, RootStackParamList, AppLoading};
+import {
+  useFonts,
+  Roboto_400Regular as Regular,
+  Roboto_500Medium as Medium,
+  Roboto_700Bold as Bold,
+} from "@expo-google-fonts/roboto";
+
+export {
+  ColoredPlaceholderTextInput,
+  ImageBackground,
+  Keyboard,
+  SafeAreaView,
+  FlatList,
+  TextInput,
+  Platform,
+  useRef,
+  KeyboardAvoidingView,
+  Regular,
+  Medium,
+  Bold,
+  useFonts,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TouchableOpacity,
+  Image,
+  React,
+  useState,
+  useEffect,
+  colors,
+  BlueButton,
+  WhiteButton,
+  useTranslation,
+  useNavigation,
+  StackNavigationProp,
+  RootStackParamList,
+  AppLoading,
+};
